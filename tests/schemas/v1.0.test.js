8e548a60d06df31e726e2e5498e1a6abc6648657
Víctor González Prieto <victor.gp.dev@gmail.com>
2025-05-05 13:59:17 +0200

    Add ajv to validate our schemas in tests
    We're using AJV because that's what the vscode-yaml extension uses for
    validating YAML files from a JSON Schema. [^1]
    
    We're pining it to major like they do. [^2]
    
    [^1]: https://node-modules.dev/grid#selected=ajv@8.17.1&install=vscode-yaml&why=ajv@8.17.1
    [^2]: https://github.com/redhat-developer/yaml-language-server/blob/main/package.json#L29

--- tests/schemas/v1.0.test.js
+++ tests/schemas/v1.0.test.js
@@ -1,5 +1,25 @@
 import { expect, test } from "vitest";
+import Ajv from "ajv"; // JSON Schema draft-07 by default
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+test("ajv example works", () => {
+  const ajv = new Ajv();
+
+  const schema = {
+    type: "object",
+    properties: {
+      foo: { type: "integer" },
+      bar: { type: "string" },
+    },
+    required: ["foo"],
+    additionalProperties: false,
+  };
+
+  const validate = ajv.compile(schema);
+
+  const data = {
+    foo: 1,
+    bar: "abc",
+  };
+
+  expect(validate(data)).toBe(true);
 });
