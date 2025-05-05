0b37c0495cded2a8128d6aec2314a139b34f63fb
Víctor González Prieto <victor.gp.dev@gmail.com>
2025-05-05 14:43:12 +0200

    Add example test for validation failure

--- tests/schemas/v1.0.test.js
+++ tests/schemas/v1.0.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import Ajv from "ajv"; // JSON Schema draft-07 by default
 
-test("ajv example works", () => {
+test("correct schema validates ok", () => {
   const ajv = new Ajv();
 
   const schema = {
@@ -22,4 +22,29 @@ test("ajv example works", () => {
   };
 
   expect(validate(data)).toBe(true);
+});
+
+test("incorrect schema validates with error", () => {
+  const ajv = new Ajv();
+
+  const schema = {
+    type: "object",
+    properties: {
+      foo: { type: "string" },
+      bar: { type: "string" },
+    },
+    required: ["foo"],
+    additionalProperties: false,
+  };
+
+  const validate = ajv.compile(schema);
+
+  const data = {
+    foo: 1,
+    bar: "abc",
+  };
+
+  expect(validate(data)).toBe(false);
+  expect(validate.errors[0].instancePath).toBe('/foo');
+  expect(validate.errors[0].message).toBe('must be string');
 });
